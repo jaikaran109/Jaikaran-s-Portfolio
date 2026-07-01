e8b1dbf13a9be1db733d2baa1e1618ae7b452ff8
jaikaran109 <jaik05581@gmail.com>
2026-07-02 02:05:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".multiple-text",{
+    strings : ["Frontend Developer" , "Coder" , "Editor" , "Creator"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay : 1000,
+    loop : true
+})
+
+
+// This thing is done by the Typed.js CDN  
